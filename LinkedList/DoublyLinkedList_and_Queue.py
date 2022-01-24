4b50bae12196a8123c15e0e9ddba25c829eb8ff3
Han, Junhui <hhjjhh601@naver.com>
2022-01-25 00:08:27 +0900

    'part queue deleted.'

--- LinkedList/DoublyLinkedList_and_Queue.py
+++ LinkedList/DoublyLinkedList_and_Queue.py
@@ -102,23 +102,4 @@ class DoublyLinkedList:
         self.nodeCount += L.nodeCount
 
 
-class LinkedListQueue:
 
-    def __init__(self):
-        self.data = DoublyLinkedList()
-
-    def size(self):
-        return self.data.nodeCount
-
-    def isEmpty(self):
-        return self.data.nodeCount == 0
-
-    def enqueue(self, item):
-        node = Node(item)
-        return self.data.insertAt(self.data.nodeCount + 1, node)
-
-    def dequeue(self):
-        return self.data.popAt(1)
-
-    def peek(self):
-        return self.data.head.next.data
